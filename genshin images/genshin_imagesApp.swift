9f9ef381c1cf40e4e2c4a90aef86539bf5271d50
Kirill Panichkin <unbearable.panic@gmail.com>
2026-04-08 00:24:38 +0400

    Initial Commit

---
+++ genshin images/genshin_imagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  genshin_imagesApp.swift
+//  genshin images
+//
+//  Created by Кирилл Паничкин on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct genshin_imagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
